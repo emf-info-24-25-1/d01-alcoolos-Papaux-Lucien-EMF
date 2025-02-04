dd0187872202255fe7c378411e0e5e322809d7e0
Papaux-Lucien-EMF <Lucien.papaux@studentfr.ch>
2025-02-04 13:55:44 +0100

    Correction du devoir01

--- src/app/Application.java
+++ src/app/Application.java
@@ -3,29 +3,46 @@ package app;
 public class Application {
         public static void main(String[] args) {
 
+                // Déclaration d'un objet de type Ami
                 Ami ami1;
                 Ami ami2;
                 Ami ami3;
 
+                //Instanciation de l'objet
                 ami1 = new Ami("Jean", 100);
 
                 ami2 = new Ami("Lucien", 10);
 
                 ami3 = new Ami("Gabriel", 1);
 
-                
-
-                System.out.println(ami1.getNom() + " peut encore boire " + ami1.getBieres() + " bières.");
-                System.out.println(ami2.getNom() + " peut encore boire " + ami2.getBieres() + " bières.");
-                System.out.println(ami3.getNom() + " peut encore boire " + ami3.getBieres() + " bières.");
+                //Utilisation de la méthode boitUneBiere
 
                 ami1.boitUneBiere();
                 ami2.boitUneBiere();
                 ami3.boitUneBiere();
-
-                System.out.println(ami1.getNom() + " peut encore boire " + ami1.getBieres() + " bières.");
-                System.out.println(ami2.getNom() + " peut encore boire " + ami2.getBieres() + " bières.");
-                System.out.println(ami3.getNom() + " peut encore boire " + ami3.getBieres() + " bières.");
+                ami1.boitUneBiere();
+                ami2.boitUneBiere();
+                ami3.boitUneBiere();
+                ami1.boitUneBiere();
+                ami2.boitUneBiere();
+                ami3.boitUneBiere();
+                ami1.boitUneBiere();
+                ami2.boitUneBiere();
+                ami3.boitUneBiere();
+                ami1.boitUneBiere();
+                ami2.boitUneBiere();
+                ami3.boitUneBiere();
+                ami1.boitUneBiere();
+                ami2.boitUneBiere();
+                ami3.boitUneBiere();
+                ami1.boitUneBiere();
+                ami2.boitUneBiere();
+                ami3.boitUneBiere();
+                
+                System.out.println("Le nombre de bière supportable pour " + ami1.getNom() + " est de " + ami1.getNmbreBieresSupportable());
+                System.out.println("Le nombre de bière supportable pour " + ami2.getNom() + " est de " + ami2.getNmbreBieresSupportable());
+                System.out.println("Le nombre de bière supportable pour " + ami3.getNom() + " est de " + ami3.getNmbreBieresSupportable());
+                
 
         }
 }
